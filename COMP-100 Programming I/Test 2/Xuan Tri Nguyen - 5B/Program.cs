7b42c14c52f8c6819852a16f61e87d06ba0977aa
Xuan Tri Nguyen <143986810+irtnaux0910@users.noreply.github.com>
2024-08-17 00:12:13 -0400

    Add files via upload

---
+++ COMP-100 Programming I/Test 2/Xuan Tri Nguyen - 5B/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Data;
+using System.Data.Common;
+
+namespace Xuan_Tri_Nguyen___5B
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int row, col;
+            Console.Write("Number of rows: ");
+            row= Convert.ToInt32(Console.ReadLine());
+            Console.Write("Number of columns: ");
+            col = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    if (i % 5 == 0|| j % 5 == 0)
+                    {
+                        Console.Write("+");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }                      
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
