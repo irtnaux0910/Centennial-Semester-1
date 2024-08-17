44b51f47de64f928859d74522eb278c73203f4ca
Xuan Tri Nguyen <143986810+irtnaux0910@users.noreply.github.com>
2024-08-17 00:11:36 -0400

    Add files via upload

---
+++ COMP-100 Programming I/Test 2/Xuan Tri Nguyen - 3B/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Xuan_Tri_Nguyen___3B
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int startValue;
+            Console.Write("Enter the start value: ");
+            startValue = Convert.ToInt32(Console.ReadLine());            
+            for (int i = startValue; i >= 0; i -= 7)
+            {                
+                Console.Write(i + " ");                
+                if (i <=7)
+                {
+                    Console.WriteLine($"\nIt will take {startValue/7+1} decrements will reach {i - 7}");
+                }
+            }            
+        }
+    }
+}
