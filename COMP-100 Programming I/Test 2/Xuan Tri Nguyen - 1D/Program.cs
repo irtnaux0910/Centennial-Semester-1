cd4b6a0c0b7ae9d2da1e5ea20f28f857fba52869
Xuan Tri Nguyen <143986810+irtnaux0910@users.noreply.github.com>
2024-08-17 00:07:44 -0400

    Add files via upload

---
+++ COMP-100 Programming I/Test 2/Xuan Tri Nguyen - 1D/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace Xuan_Tri_Ngueyn___1D
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int mult, value;
+            Console.Write("Enter the multiple: ");
+            mult = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter start value: ");
+            value = Convert.ToInt32(Console.ReadLine());                  
+            do
+            {                
+                if (value % mult == 0)
+                {
+                    Console.Write(value + " ");
+                }
+                value--;
+            } while (value >= 1);
+        }
+    }
+}
