e542d6df6f6c7aadd59d0cd1da0beeb9c7be502c
Xuan Tri Nguyen <143986810+irtnaux0910@users.noreply.github.com>
2024-08-17 00:11:17 -0400

    Add files via upload

---
+++ COMP-100 Programming I/Test 2/Xuan Tri Nguyen - 2B/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Xuan_Tri_Nguyen___2B
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int endGram, startGram;
+            Console.Write("Enter the start gram value: ");
+            startGram = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the end gram value: ");
+            endGram = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Grams\tOunces");            
+            while (startGram >= endGram)
+            {
+                double ounces = startGram * 0.035;
+                Console.WriteLine($"{startGram}\t{ounces:F3}");
+                startGram -= 10;
+            }
+        }
+    }
+}
